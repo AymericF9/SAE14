d6e108dec901694ded52893ef182fd0c460a4983
AymericF9 <95702319+AymericF9@users.noreply.github.com>
2021-12-09 10:59:02 +0100

    Initial  commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="Page 1")
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+	/** 
+     * @Route("/", name="home") 
+	*/ 
+	public function home()  
+	{ 
+	return $this->render('blog/home.html.twig', [ 
+	'title' => "bienvenuueeeee", 
+	]); 
+}
+	/**
+     * @Route("/blognew", name="Page 2")
+     */
+	public function index2() : Response
+	{ 
+	return $this->render('blognew/page2.html.twig'); 
+}
+}
